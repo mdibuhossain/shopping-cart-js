24bc72dd710e49719786912ec29158a0f6b49562
Ibrahim H <mohammad.ihakash@gmail.com>
2021-08-19 03:18:13 +0600

    line reduse

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ function updatePrice(isIncrease, product, price) {
     const productCount = document.getElementById(product + '-qnt');
     let productCountValue = parseInt(productCount.value);
     const productPrice = document.getElementById(product + '-price');
-    const productPriceValue = parseFloat(productPrice.innerText);
+    let productPriceValue = parseFloat(productPrice.innerText);
     if (isIncrease) {
         productCountValue++;
-        productCount.value = productCountValue;
-        productPrice.innerText = price * productCountValue
     }
     else if (productCountValue > 0) {
         productCountValue--;
-        productCount.value = productCountValue;
-        productPrice.innerText = price * productCountValue
     }
+    productCount.value = productCountValue;
+    productPriceValue = price * productCountValue
+    productPrice.innerText = productPriceValue;
+    chekOutPrice();
 }
 
+function chekOutPrice() {
+
+}
 
 document.getElementById('phone-plus').addEventListener('click', function () {
     updatePrice(true, 'phone', 1219);
